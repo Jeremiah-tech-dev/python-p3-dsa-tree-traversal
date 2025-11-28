be8a211320c413f0b44768ae48911d851649eeef
Jeremiah-tech-dev <jeremiah.koske@student.moringaschool.com>
2025-11-28 15:24:24 +0300

    Did the Tree lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,21 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    # Depth-first traversal using a stack
+    if self.root is None:
+      return None
+    
+    stack = [self.root]
+    
+    while stack:
+      node = stack.pop()
+      
+      if node.get('id') == id:
+        return node
+      
+      # Add children to stack in reverse order to maintain left-to-right traversal
+      if 'children' in node and node['children']:
+        for child in reversed(node['children']):
+          stack.append(child)
+    
+    return None
